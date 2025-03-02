dfad7a721083e0fb68b965e3a5576d3d070ddcd1
taewoocode <qkrxodn6035@gmail.com>
2025-03-02 23:51:34 +0900

    Connection test

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,11 @@
+package hello.jdbc.connection;
+
+/**
+ * abstract로 객체생성 막음
+ */
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
+
---
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -0,0 +1,29 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static hello.jdbc.connection.ConnectionConst.PASSWORD;
+import static hello.jdbc.connection.ConnectionConst.USERNAME;
+
+@Slf4j
+public class DBConnectionUtil {
+
+    public static Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            log.info("get connection={}", "class={}", connection, connection.getClass());
+            return connection;
+        } catch (SQLException e) {
+
+            /**
+             * RunTimeException
+             */
+            throw new IllegalStateException(e);
+        }
+    }
+}
