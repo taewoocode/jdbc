c113ee976deeb1d6afd9c83e2a737ecd7e498aa8
taewoocode <qkrxodn6035@gmail.com>
2025-03-04 23:33:43 +0900

    업데이트 기능 추가

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,20 +1,34 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+        Member member = new Member("memberV2", 10000);
         repository.save(member);
 
+        //findMember
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        log.info("member != findMember {}", member == findMember);
+        assertThat(findMember).isEqualTo(member);
+
+        //update: money 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updateMember = repository.findById(member.getMemberId());
+        assertThat(updateMember).isEqualTo(member);
     }
 }
